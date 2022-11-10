2484d3ead4fcd32f63bb468e93bcf189a606dddd
John Pae <johns.regester@gmail.com>
2022-11-10 17:04:25 -0500

    special zero case

--- rst/src/main.rs
+++ rst/src/main.rs
@@ -1,6 +1,14 @@
 fn main()
 {
-    _ = read_line_as_num().expect("input provided was not valid and could not be read");
+    //let val: f32 = read_line_as_num().expect("input provided was not valid and could not be read");
+    if let Some(val) = read_line_as_num()
+    {
+        println!("{}", arabic_to_roman(val));
+    }
+    else
+    {
+        panic!("Input provided was not valid");
+    }
 }
 
 fn read_line_as_num() -> Option<f32>
@@ -22,4 +30,11 @@ fn parse_line_as_num(line: String) -> Option<f32>
         Ok(val) => {println!("Parsed {line} as {val}."); Some(val)},
         Err(err) => {println!("Error found: {err} while parsing input as number."); None}
     }
+}
+
+fn arabic_to_roman(val: f32) -> String
+{
+    if val == 0.0 {return "Z".to_string();}
+
+    todo!();
 }
