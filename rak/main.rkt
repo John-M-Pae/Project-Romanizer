2fa31511a6db67c1e7eae8f35e026a7fb1efa3cd
John Pae <johns.regester@gmail.com>
2022-12-05 17:12:08 -0500

    Add value validation

--- rak/main.rkt
+++ rak/main.rkt
@@ -1,9 +1,12 @@
 #lang racket
 
-(provide rkt-main)
+(provide rak-main)
 
-(define (rkt-main)
-    (get-line))
+(define (rak-main)
+    (let [[value (get-line)]]
+        (if (is-valid? value)
+            (roman value)
+            (error "Your value is not a valid Roman numeral"))))
 
 (define (get-line)
     (display "Enter your number:\n")
@@ -14,3 +17,13 @@
         (if line
             line
             (error "Invalid input string given. Expected a number."))))
+
+(define (is-valid? val)
+    (let [[test-val (* 12 val)]
+          [bound 2399999]]
+        (and (integer? test-val)
+            (<= test-val
+                bound))))
+
+(define (roman val)
+    val)
