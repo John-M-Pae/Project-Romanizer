897167befc3520b1956957371fd4ba3f79491c93
John Pae <johns.regester@gmail.com>
2022-12-05 12:14:03 -0500

    Basic data validation and handle the zero case

--- jul/src/jul.jl
+++ jul/src/jul.jl
@@ -1,7 +1,11 @@
 module jul
 
 function main()
-    read_number_in()
+    value = read_number_in()
+    if !valid(value)
+        error("Invalid input")
+    end
+    print("Your value in roman numerals is: $(roman(value))")
 end
 
 function read_number_in()
@@ -11,6 +15,15 @@ function read_number_in()
     val = parse(Float32, line)
     println("value is interpreted as $(val)")
     return val
+end
+
+valid(val) = isinteger(12val) && 12val <= 12(10^5 + 9* +(10^4, 10^3, 10^2, 10, 1) + 11)
+
+function roman(val)
+    if val == 0
+        return 'Z'
+    end
+
 end
 
 end
