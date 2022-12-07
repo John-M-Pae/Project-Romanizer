835ffc6f01085f69c5191ec75370a5076ff360b6
John Pae <johns.regester@gmail.com>
2022-12-07 14:17:28 -0500

    Simple tabular implementation

--- rst/src/main.rs
+++ rst/src/main.rs
@@ -1,14 +1,10 @@
 fn main()
 {
-    //let val: f32 = read_line_as_num().expect("input provided was not valid and could not be read");
-    if let Some(val) = read_line_as_num()
-    {
-        println!("{}", arabic_to_roman(val));
-    }
-    else
-    {
-        panic!("Input provided was not valid");
-    }
+    let val: f32 = read_line_as_num().expect("input provided was not valid and could not be read");
+
+    if !is_valid(val) {panic!("Input cannot be interpreted as a Roman numeral.");}
+
+    println!("{}", arabic_to_roman(val));
 }
 
 fn read_line_as_num() -> Option<f32>
@@ -32,6 +28,13 @@ fn parse_line_as_num(line: String) -> Option<f32>
     }
 }
 
+fn is_valid(value: f32) -> bool
+{
+    let test_val = 12.0*value;
+    let bound = 12*(100_000 + 9*(10_000 + 1000 + 100 + 10 + 1)) + 11;
+    (value >= 0.0) && (test_val <= bound as f32) && (test_val.fract() == 0.0)
+}
+
 fn arabic_to_roman(value: f32) -> String
 {
     if value == 0.0 {return "Z".to_string();}
@@ -39,6 +42,14 @@ fn arabic_to_roman(value: f32) -> String
     let mut val = value;
 
     let numerals = [
+        Numeral{value: 100_000.0, symbol: "ↈ"},
+        Numeral{value: 50_000.0, symbol: "ↇ"},
+        Numeral{value: 10_000.0, symbol: "ↂ"},
+        Numeral{value: 5000.0, symbol: "ↁ"},
+        Numeral{value: 1000.0, symbol: "ↀ"},
+        Numeral{value: 900.0, symbol: "Cↀ"},
+        Numeral{value: 500.0, symbol: "D"},
+        Numeral{value: 400.0, symbol: "CD"},
         Numeral{value: 100.0, symbol: "C"},
         Numeral{value: 90.0, symbol: "XC"},
         Numeral{value: 50.0, symbol: "L"},
@@ -47,7 +58,13 @@ fn arabic_to_roman(value: f32) -> String
         Numeral{value: 9.0, symbol: "IX"},
         Numeral{value: 5.0, symbol: "V"},
         Numeral{value: 4.0, symbol: "IV"},
-        Numeral{value: 1.0, symbol: "I"}
+        Numeral{value: 1.0, symbol: "I"},
+        Numeral{value: 0.5, symbol: "S"},
+        Numeral{value: 5.0/12.0, symbol: "⁙"},
+        Numeral{value: 4.0/12.0, symbol: "∷"},
+        Numeral{value: 3.0/12.0, symbol: "∴"},
+        Numeral{value: 2.0/12.0, symbol: ":"},
+        Numeral{value: 1.0/12.0, symbol: "·"}
     ];
 
     let mut result = String::new();
